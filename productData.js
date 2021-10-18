af428fe6ea750e56a1f9d0996ee9af5c8f352a24
Saurabh Pratap Singh <singhsaurabhpratap514@gmail.com>
2021-10-18 11:16:56 +0530

    adding files

---
+++ productData.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: "1",
+    name: "PhotoShop",
+    price: 300,
+  },
+  {
+    id: "2",
+    name: "Webstorm",
+    price: 300,
+  },
+  {
+    id: "3",
+    name: "Figma",
+    price: 300,
+  },
+];
+
+module.exports = products;
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const apiKeyMiddleware = require("../middlewares/apiKey");
+
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "My home page",
+  });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", {
+    title: "My about page",
+  });
+});
+
+router.get("/download", (req, res) => {
+  res.download(path.resolve(__dirname) + "/about.html");
+});
+
+// router.get("/api/products", apiKeyMiddleware, (req, res) => {
+//   res.json([
+//     {
+//       id: "123",
+//       name: "Chrome",
+//     },
+//     {
+//       id: "235",
+//       name: "firefox",
+//     },
+//   ]);
+// });
+
+module.exports = router;
